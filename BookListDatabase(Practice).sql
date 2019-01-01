27394286845cf11ea6c90095e7b9c30912caa09c
Jaskirat Singh Gill <46098063+JaskiratGill@users.noreply.github.com>
2019-01-01 17:46:49 -0500

    Update BookListDatabase(Practice).sql

--- BookListDatabase(Practice).sql
+++ BookListDatabase(Practice).sql
@@ -4,9 +4,6 @@
 
 CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
 
-
-CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER );
-
 INSERT INTO books VALUES (1, "First book", 5);
 INSERT INTO books VALUES (2, "Second book", 3);
 INSERT INTO books VALUES (3, "Third book", 4);
